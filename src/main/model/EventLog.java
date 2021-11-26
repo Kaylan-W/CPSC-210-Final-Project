d5edb739b595eaaa188e6bf3464330f5e63e39d8
kaylanwallace <kaylanwallace6gmail.com>
2021-11-26 08:23:46 -0800

    Phase 4: Task 2 Fixed Checkstyle errors

--- src/main/model/EventLog.java
+++ src/main/model/EventLog.java
@@ -1,59 +1,11 @@
 package model;
 
-
-//import java.util.ArrayList;
-//import java.util.Collection;
-//import java.util.Iterator;
-//
-///**
-// * Represents a log of alarm system events.
-// */
-//public class EventLog implements Iterable<Event> {
-//    /** the only EventLog in the system (Singleton Design Pattern) */
-//    private static EventLog theLog;
-//    private Collection<Event> events;
-//
-//    //EFFECTS: Prevent external construction.
-//    private EventLog() {
-//        events = new ArrayList<>();
-//    }
-//
-//    // EFFECTS: Gets instance of EventLog - creates it if it doesn't already exist.
-//    // @return  instance of EventLog
-//    public static EventLog getInstance() {
-//        if (theLog == null)
-//            theLog = new EventLog();
-//
-//        return theLog;
-//    }
-//
-//    /**
-//     * Adds an event to the event log.
-//     * @param e the event to be added
-//     */
-//    public void logEvent(Event e) {
-//        events.add(e);
-//    }
-//
-//    // EFFECTS: Clears the event log and logs the event.
-//    public void clear() {
-//        events.clear();
-//        logEvent(new Event("Event log cleared."));
-//    }
-//
-//    @Override
-//    public Iterator<Event> iterator() {
-//        return events.iterator();
-//    }
-//}
-
-
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 
 /**
- * Represents a log of alarm system events.
+ * Represents a log of events.
  * We use the Singleton Design Pattern to ensure that there is only
  * one EventLog in the system and that the system has global access
  * to the single instance of the EventLog.
@@ -78,9 +30,9 @@ public class EventLog implements Iterable<Event> {
      * @return  instance of EventLog
      */
     public static EventLog getInstance() {
-        if (theLog == null)
+        if (theLog == null) {
             theLog = new EventLog();
-
+        }
         return theLog;
     }
 
